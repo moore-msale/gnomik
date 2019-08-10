961f9c803f699602551657dc7bdafa86395d731c
uki7991 <tilek.kubanov@gmail.com>
2019-08-10 20:13:36 +0600

    Update photo.blade.php

--- resources/views/photo.blade.php
+++ resources/views/photo.blade.php
@@ -3,19 +3,19 @@
 @section('content')
     <form action="/photo" method="POST" style="padding: 20%;" enctype="multipart/form-data">
         @csrf
-        <select class="tagsinput" data-role="tagsinput" name="tags[]" multiple>
+        <select multiple data-role="tagsinput" name="tags[]">
         </select>
         <button type="submit">Submit</button>
     </form>
 @stop
 
 @push('styles')
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.bundle.min.js"></script>
+
     <link rel="stylesheet" href="{{ asset('css/tagsinput.css') }}">
 @endpush
 
 @push('scripts')
-    <script src="{{ asset('js/tagsinput.min.js') }}"></script>
-    <script>
-        $('.tagsinput').tagsinput();
-    </script>
+    <script src="{{ asset('js/tagsinput.js') }}"></script>
 @endpush
