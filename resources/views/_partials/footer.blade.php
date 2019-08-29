60cd7327caa747aae013ea1a669403aac9e413d0
nurizzzka <nuriza.akynbaeva@mail.ru>
2019-08-29 11:57:41 +0600

    Add icons and change images

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -7,4 +7,12 @@
         <div class="col-lg-3 col-md-3 col-12 text-center"><p class="text-fut-light" style="font-size: 20px;line-height: 28px;color: #FFFFFF;">Все права защищены
                 <br>© 2019</p></div>
     </div>
+    <div>
+        <a class="call-a d-md-block d-none" href="tel: +996557088444">
+            <i class="call-ic fa fa-phone fa-2x"></i>
+        </a>
+        <a class="wa-a d-md-block d-none" href="https://api.whatsapp.com/send?phone=996556759444">
+            <i class="wa-ic fab fa-whatsapp fa-2x"></i>
+        </a>
+    </div>
 </div>
