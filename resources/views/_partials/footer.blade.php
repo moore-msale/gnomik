99461bb0536d8c7078300625043356b904ad1c3e
nurizzzka <nuriza.akynbaeva@mail.ru>
2019-09-06 15:58:53 +0600

    asd

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -19,8 +19,8 @@
         <li class="share top">
             <i class="fas fa-comments main-co"  style="font-size:28px;"></i>
             <ul class="submenu">
-                <li><a href="tel: +996556759444" class="viber"><span>Телефон</span><i class="fas fa-phone-square" style="font-size:28px; z-index:999;"></i></a></li>
                 <li><a href="https://wa.me/+996556759444" class="whatsapp"><span>Whatsapp</span><i class="fab fa-whatsapp" style="font-size:28px; z-index:999;"></i></a></li>
+                <li><a href="tel: +996556759444" class="viber"><span>Телефон</span><i class="fas fa-phone-square" style="font-size:28px; z-index:999;"></i></a></li>
 
                 {{--                <li><a href="https://t.me/akmaral_issykkul" class="telegram"><span>Telegram</span><i class="fab fa-telegram" style="font-size:28px; z-index:999;"></i></a></li>--}}
 {{--                <li><a href="#ulp-xRcLEhGJEuCyb9pQ" class="bid"><span>Оставьте заявку</span><i class="fas fa-comment" style="font-size:28px; z-index:999;"></i></a></li>--}}
